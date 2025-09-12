1e2440e2eaf11479bac3f782ef8fd0e7faf98afe
luienneguedes1-alt <luienneguedes1@gmail.com>
2025-09-12 10:20:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
-const mensagens = [
-   "Você é mais forte do que imagina 💛",
-   "Nunca se esqueça:sua vida importa 🌻",
-   "Você não esta sozinho, estamos juntos 🤝",
-   "Acredite: dias melhores virão ⭐",
-   "Sempre existe esperança 🌈",
-   "Sua vida importa ❤️​",
-   "Falar é a melhor solução ", 
-   "Peça ajuda, você merece 🗣",
-   "A vida sempre vale a pena 🌸",
-   "Cuide de si mesmo(a) 😊",
-   "A esperança é a luz 🤍",
-"Seu silêncio não precisa ser sua dor 🤗"
-   
-];​
+  const mensagens = [
+      "Você é mais forte do que imagina 💛",
+      "Nunca se esqueça:sua vida importa 🌻",
+      "Você não está sozinho, estamos juntos 🤝",
+      "Acredite: dias melhores virão ⭐",
+      "Sempre existe esperança 🌈",
+      "Sua vida importa ❤️​",
+      "Falar é a melhor solução ", 
+      "Peça ajuda, você merece 🗣",
+      "A vida sempre vale a pena 🌸",
+      "Cuide de si mesmo(a) 😊",
+      "A esperança é a luz 🤍",
+      "Seu silêncio não precisa ser sua dor 🤗"
+    ];
 
-document.getElementById("btnMensagem").addEventListener("click",() => {
-  const aleatoria = mensagens[Math.floor(Math.random() * mensagens.length)];
-  document.getElementById("mensagem").innerText = aleatoria;
-});
+    document.getElementById("btnMensagem").addEventListener("click", () => {
+      const aleatoria = mensagens[Math.floor(Math.random() * mensagens.length)];
+      document.getElementById("mensagem").innerText = aleatoria;
+    });
+  </script>
+
